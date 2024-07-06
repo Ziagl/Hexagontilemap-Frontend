e6b96776ae610214c6fa337cbb4033c4fe75145e
Ziagl <office@ziegelwanger-edv.at>
2024-07-06 21:50:28 +0200

    added IUnit from package

--- src/models/Unit.ts
+++ src/models/Unit.ts
@@ -1,6 +1,8 @@
 import { Scene } from "phaser";
+import { IUnit } from '@ziagl/tiled-map-units';
+import { CubeCoordinates } from "honeycomb-grid";
 
-export class Unit extends Phaser.GameObjects.Image
+export class Unit extends Phaser.GameObjects.Image implements IUnit
 {
     constructor (scene:Scene, x:number, y:number, texture:string)
     {
@@ -8,4 +10,18 @@ export class Unit extends Phaser.GameObjects.Image
 
         // TODO
     }
+    
+    // properties from IUnit
+    unitId: number;
+    unitPosition: CubeCoordinates;
+    unitPlayer: number;
+    unitType: number;
+    unitHealth: number;
+    unitMaxHealth: number;
+    unitMovement: number;
+    unitAttack: number;
+    unitDefense: number;
+    unitRange: number;
+    canAttack: boolean;
+    canMove: boolean;
 }
